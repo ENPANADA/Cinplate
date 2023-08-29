b8a7c117c1ed080db3e0562a7c7bb18478b85b5c
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2023-08-29 16:26:53 -0600

    Tarea 1

--- main.js
+++ main.js
@@ -7,13 +7,30 @@ let Page = 1;
 
 let Peliculas;
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzVlMTRjODNkZWFhZDcwNzMwZTcyM2UzMzNmZTExYyIsInN1YiI6IjY0ZWI5ZjkzNDU4MTk5MDExZDJiYWZlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h5BnD7izsFV35t8eF5Ywu2hiGwQBgeHi5B-oWrzXK78'
+  }
+};
+
 function CargarPeliculas(){
+  /*
   fetch(`${API_PATH}/discover/movie/?api_key=${API_KEY}&page=${Page}`)
   .then(response => response.json())
     .then(function(data){
       AgregarPeliculas(data.results)
     })
     .catch(error => console.log('Error: ',error));
+*/
+    
+    fetch(`https://api.themoviedb.org/3/discover/movie?page=${Page}&sort_by=popularity.desc`, options)
+      .then(response => response.json())
+      .then(function(data){
+        AgregarPeliculas(data.results)
+      })
+      .catch(err => console.error(err));
 }
 
 function AgregarPeliculas(data){
